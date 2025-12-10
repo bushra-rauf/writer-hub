bfc31059eab36d06eefca4340af4c2c028a3334f
bushra-rauf <bush_rauf@yahoo.com>
2025-12-10 02:52:04 +0100

    added  queries.ts  Moved query and write builtin types to queries.ts, Created HomeBooks component using exported types, Mapped books with slug links and styling, Rendered component on homepage with fetched data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-import { createClient } from "../utils/supabase/browserClient";
+import HomeBooks from "@/components/HomeBooks";
+import { getHomeBooks } from "@/utils/supabase/queries";
+
 
 export default async function Home() {
-  const supabase = createClient ()
-  const {data, error} = await supabase.from('books')
-                                      .select('id, slug, title, writers("name")' )
-                                       .order("created_at", {ascending: false})
+  const {data, error} = await getHomeBooks()
 
   console.log ('data:' + data , 'error' + error?.message)
   return (
     <div className="w-80 mx-auto">
-      Hi world!
-      {data && data.map(({id, slug, title, writers })=> (<div key={id}>{title} By: {writers.name}</div>))}
+       <HomeBooks books={data!}/>
     </div>
   );
 }
---
+++ src/components/HomeBooks/index.tsx
@@ -0,0 +1,17 @@
+import { HomeBooksType } from "@/utils/supabase/queries"
+import Link from "next/link"
+
+const HomeBooks = ({books}: {books: HomeBooksType}) => {
+    return (
+        <div>
+           {books && books.map(({id, slug, title, writers })=> (
+            <Link key={id} href={`/${slug}`}><div className=" border-1 rounded p-4 mt-4">
+                <h2 className="font-extrabold text-xl">{title}</h2>
+                <div className="text-right"> By: {writers.name}</div>
+                </div>
+            </Link>))}
+        </div>
+    )
+}
+
+export default HomeBooks
