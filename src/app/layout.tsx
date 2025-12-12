1c5534e81964421cc6f722c18c98de450324101a
bushra-rauf <bush_rauf@yahoo.com>
2025-12-12 02:08:35 +0100

    install npm i @tanstack/react-query, created file query-client-provider under src and add it in the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-
+import {QueryClientProvider} from "@/providers/query-client-provider"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,8 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <QueryClientProvider>
         <Header/>
         {children}
+        </QueryClientProvider>
       </body>
     </html>
   );
