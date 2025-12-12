69dd3e92da4bcdd84126de36956d4b2540e3162a
bushra-rauf <bush_rauf@yahoo.com>
2025-12-12 04:04:08 +0100

    added useQuery on homepost component to see after added rows in books table how it show directly on front-end

--- src/components/HomeBooks/index.tsx
+++ src/components/HomeBooks/index.tsx
@@ -1,11 +1,24 @@
-import { HomeBooksType } from "@/utils/supabase/queries"
+"use client"
+import { getHomeBooks, HomeBooksType } from "@/utils/supabase/queries"
 import Link from "next/link"
+import { useQuery } from "@tanstack/react-query"
 
 const HomeBooks = ({books}: {books: HomeBooksType}) => {
+    const {data} = useQuery({
+        queryKey:["home-books"],
+        queryFn: async() => {
+            const {data,error} = await getHomeBooks()
+            if(error) throw error 
+            return data
+        },
+        initialData: books,
+        refetchOnMount: false,
+        staleTime: 10000 * 10
+    })
     return (
         <div>
-           {books && books.map(({id, slug, title, writers })=> (
-            <Link key={id} href={`/${slug}`}><div className="block border-1 rounded p-4 mt-4">
+           {data && data.map(({id, slug, title, writers })=> (
+            <Link key={id} href={`/${slug}`}><div className="block border-1 max-w-2xl m-auto rounded p-4 mt-4">
                 <h2 className="font-extrabold text-xl">{title}</h2>
                 <div className="text-right"> By: {writers.name}</div>
                 </div>
